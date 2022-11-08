08815477f3d8c3d3e44d536db8ab7dbf98fdcde3
Innoberger <fynn.arnold@pm.me>
2022-11-08 15:01:26 +0100

    fixed at-most-one functions

--- main.cpp
+++ main.cpp
@@ -17,19 +17,19 @@ std::ostream &operator<<(std::ostream &os, const std::list<int> &list) {
 std::list<std::list<int>> at_most_one_col_indices() {
     std::list<std::list<int>> at_most_one_indices;
 
-    // iterate over each column
-    for (unsigned int i = 0; i < 9; i++) {
-        std::list<int> column_clauses;
-        // iterate over each row
-        for (unsigned int j = 0; j < 9; j++) {
-            // iterate over each number
-            for (unsigned int n = 1; n <= 9; n++) {
+    // iterate over each number
+    for (unsigned int n = 1; n <= 9; n++) {
+        // iterate over each column
+        for (unsigned int i = 0; i < 9; i++) {
+            std::list<int> column_clauses;
+            // iterate over each row
+            for (unsigned int j = 0; j < 9; j++) {
                 // construct the index for sat variable
                 unsigned int var_index = n * 100 + i * 10 + j;
                 column_clauses.push_back(var_index);
             }
+            at_most_one_indices.push_back(column_clauses);
         }
-        at_most_one_indices.push_back(column_clauses);
     }
 
     return at_most_one_indices;
@@ -41,19 +41,19 @@ std::list<std::list<int>> at_most_one_col_indices() {
 std::list<std::list<int>> at_most_one_row_indices() {
     std::list<std::list<int>> at_most_one_indices;
 
-    // iterate over each row
-    for (unsigned int j = 0; j < 9; j++) {
-        std::list<int> row_clauses;
-        // iterate over each column
-        for (unsigned int i = 0; i < 9; i++) {
-            // iterate over each number
-            for (unsigned int n = 1; n <= 9; n++) {
+    // iterate over each number
+    for (unsigned int n = 1; n <= 9; n++) {
+        // iterate over each row
+        for (unsigned int j = 0; j < 9; j++) {
+            std::list<int> row_clauses;
+            // iterate over each column
+            for (unsigned int i = 0; i < 9; i++) {
                 // construct the index for sat variable
                 unsigned int var_index = n * 100 + i * 10 + j;
                 row_clauses.push_back(var_index);
             }
+            at_most_one_indices.push_back(row_clauses);
         }
-        at_most_one_indices.push_back(row_clauses);
     }
 
     return at_most_one_indices;
@@ -65,17 +65,17 @@ std::list<std::list<int>> at_most_one_row_indices() {
 std::list<std::list<int>> at_most_one_block_indices() {
     std::list<std::list<int>> at_most_one_indices;
 
-    // iterate over each block column
-    for (unsigned int a = 0; a < 3; a++) {
-        // iterate over each block row
-        for (unsigned int b = 0; b < 3; b++) {
-            std::list<int> block_clauses;
-            // iterate over each column in a block
-            for (unsigned int col_i = 0; col_i < 3; col_i++) {
-                // iterate over each row in a block
-                for (unsigned int row_j = 0; row_j < 3; row_j++) {
-                    // iterate over each number
-                    for (unsigned int n = 1; n <= 9; n++) {
+    // iterate over each number
+    for (unsigned int n = 1; n <= 9; n++) {
+        // iterate over each block column
+        for (unsigned int a = 0; a < 3; a++) {
+            // iterate over each block row
+            for (unsigned int b = 0; b < 3; b++) {
+                std::list<int> block_clauses;
+                // iterate over each column in a block
+                for (unsigned int col_i = 0; col_i < 3; col_i++) {
+                    // iterate over each row in a block
+                    for (unsigned int row_j = 0; row_j < 3; row_j++) {
                         // construct the index for sat variable
                         unsigned int col_offset = a * 3;
                         unsigned int row_offset = b * 3;
@@ -83,8 +83,8 @@ std::list<std::list<int>> at_most_one_block_indices() {
                         block_clauses.push_back(var_index);
                     }
                 }
+                at_most_one_indices.push_back(block_clauses);
             }
-            at_most_one_indices.push_back(block_clauses);
         }
     }
 
@@ -92,7 +92,9 @@ std::list<std::list<int>> at_most_one_block_indices() {
 }
 
 int main() {
-    std::list<std::list<int>> list = at_most_one_col_indices();
+    std::list<std::list<int>> list = at_most_one_block_indices();
     std::cout << list.front();
+    std::cout << "---" << std::endl;
+    std::cout << list.back();
     return 0;
 }
