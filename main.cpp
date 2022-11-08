c5e3ee009b325d450175634f906eb10a2cce6ad2
Innoberger <fynn.arnold@pm.me>
2022-11-08 14:21:29 +0100

    added functions to determine at-most-one indices for blocks

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int main() {
 }
 
 /*
- * Returns the AtMostOne clause x indices for each column.
+ * Returns the AtMostOne clause sat variable indices for each column.
  */
 std::list<std::list<int>> at_most_one_col_indices() {
     std::list<std::list<int>> at_most_one_indices;
@@ -19,9 +19,9 @@ std::list<std::list<int>> at_most_one_col_indices() {
         for (unsigned int j = 0; j < 9; j++) {
             // iterate over each number
             for (unsigned int n = 1; n <= 9; n++) {
-                // construct the index for x
-                unsigned int x_index = n * 100 + i * 10 + j;
-                column_clauses.push_back(x_index);
+                // construct the index for sat variable
+                unsigned int var_index = n * 100 + i * 10 + j;
+                column_clauses.push_back(var_index);
             }
         }
         at_most_one_indices.push_back(column_clauses);
@@ -29,7 +29,7 @@ std::list<std::list<int>> at_most_one_col_indices() {
 }
 
 /*
- * Returns the AtMostOne clause x indices for each row.
+ * Returns the AtMostOne clause sat variable indices for each row.
  */
 std::list<std::list<int>> at_most_one_row_indices() {
     std::list<std::list<int>> at_most_one_indices;
@@ -41,11 +41,41 @@ std::list<std::list<int>> at_most_one_row_indices() {
         for (unsigned int i = 0; i < 9; i++) {
             // iterate over each number
             for (unsigned int n = 1; n <= 9; n++) {
-                // construct the index for x
-                unsigned int x_index = n * 100 + i * 10 + j;
-                row_clauses.push_back(x_index);
+                // construct the index for sat variable
+                unsigned int var_index = n * 100 + i * 10 + j;
+                row_clauses.push_back(var_index);
             }
         }
         at_most_one_indices.push_back(row_clauses);
+    }
+}
+
+/*
+ * Returns the AtMostOne clause sat variable indices for each block.
+ */
+std::list<std::list<int>> at_most_one_block_indices() {
+    std::list<std::list<int>> at_most_one_indices;
+
+    // iterate over each block column
+    for (unsigned int a = 0; a < 3; a++) {
+        // iterate over each block row
+        for (unsigned int b = 0; b < 3; b++) {
+            std::list<int> block_clauses;
+            // iterate over each column in a block
+            for (unsigned int col_i = 0; col_i < 3; col_i++) {
+                // iterate over each row in a block
+                for (unsigned int row_j = 0; row_j < 3; row_j++) {
+                    // iterate over each number
+                    for (unsigned int n = 1; n <= 9; n++) {
+                        // construct the index for sat variable
+                        unsigned int col_offset = a * 3;
+                        unsigned int row_offset = b * 3;
+                        unsigned int var_index = n * 100 + (col_i + col_offset) * 10 + (row_j + row_offset);
+                        block_clauses.push_back(var_index);
+                    }
+                }
+            }
+            at_most_one_indices.push_back(block_clauses);
+        }
     }
 }
