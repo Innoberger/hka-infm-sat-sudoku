fa7e47eb64de5123f586b89b4f2e7440a18e01b5
Innoberger <fynn.arnold@pm.me>
2022-11-11 09:14:55 +0100

    using namespace std

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 #include <list>
 #include <math.h>
 
+using namespace std;
+
 struct encoding {
     unsigned int number, column, row;
 };
 
 /*
- * Overloads the << operator to be able to be used with std::list<unsigned int>.
+ * Overloads the << operator to be able to be used with list<unsigned int>.
  */
-std::ostream &operator<<(std::ostream &os, const std::list<unsigned int> &list) {
+ostream &operator<<(ostream &os, const list<unsigned int> &list) {
     for (auto const &i: list) {
-        os << i << std::endl;
+        os << i << endl;
     }
     return os;
 }
 
 /*
- * Returns the bit sequence of a decimal number as std::string.
+ * Returns the bit sequence of a decimal number as string.
  */
-std::string dec_to_bin(unsigned int num) {
-    std::string result;
+string dec_to_bin(unsigned int num) {
+    string result;
     while (num != 0) {
         if (num % 2)
             result.append(1, '1');
@@ -28,21 +30,21 @@ std::string dec_to_bin(unsigned int num) {
             result.append(1, '0');
         num /= 2;
     }
-    std::reverse(result.begin(), result.end());
+    reverse(result.begin(), result.end());
     return result;
 }
 
 /*
  * Returns the decimal number of a bit sequence.
  */
-unsigned int bin_to_dec(std::string num) {
-    return std::stoi(num, 0, 2);
+unsigned int bin_to_dec(string num) {
+    return stoi(num, 0, 2);
 }
 
 /*
  * Returns a string filled up with chars.
  */
-std::string pad_left(std::string source, unsigned int totalWidth, char paddingChar = ' ') {
+string pad_left(string source, unsigned int totalWidth, char paddingChar = ' ') {
     if (totalWidth > source.size())
         source.insert(0, totalWidth - source.size(), paddingChar);
 
@@ -53,11 +55,11 @@ std::string pad_left(std::string source, unsigned int totalWidth, char paddingCh
  * Returns the binary encoded number, column and row as a single integer.
  */
 unsigned int encode(unsigned int n, unsigned int i, unsigned int j, unsigned int bits) {
-    std::string num_binary = pad_left(dec_to_bin(n), bits, '0');
-    std::string col_binary = pad_left(dec_to_bin(i), bits, '0');
-    std::string row_binary = pad_left(dec_to_bin(j), bits, '0');
+    string num_binary = pad_left(dec_to_bin(n), bits, '0');
+    string col_binary = pad_left(dec_to_bin(i), bits, '0');
+    string row_binary = pad_left(dec_to_bin(j), bits, '0');
 
-    std::string combined_binary = num_binary.append(col_binary).append(row_binary);
+    string combined_binary = num_binary.append(col_binary).append(row_binary);
 
     return bin_to_dec(combined_binary);
 }
@@ -70,7 +72,7 @@ encoding decode(unsigned int encoded, unsigned int order) {
     unsigned int bits = ceil(log2(dimension));
     encoding enc;
 
-    std::string encoded_binary = pad_left(dec_to_bin(encoded), 3 * bits, '0');
+    string encoded_binary = pad_left(dec_to_bin(encoded), 3 * bits, '0');
 
     enc.number = bin_to_dec(encoded_binary.substr(0, bits));
     enc.column = bin_to_dec(encoded_binary.substr(bits, bits));
@@ -82,8 +84,8 @@ encoding decode(unsigned int encoded, unsigned int order) {
 /*
  * Returns the indices for each column.
  */
-std::list<std::list<unsigned int>> col_indices(unsigned int order) {
-    std::list<std::list<unsigned int>> at_most_one_indices;
+list<list<unsigned int>> col_indices(unsigned int order) {
+    list<list<unsigned int>> at_most_one_indices;
     unsigned int dimension = order * order;
     unsigned int bits = ceil(log2(dimension));
 
@@ -91,7 +93,7 @@ std::list<std::list<unsigned int>> col_indices(unsigned int order) {
     for (unsigned int n = 1; n <= dimension; n++) {
         // iterate over each column
         for (unsigned int i = 0; i < dimension; i++) {
-            std::list<unsigned int> column_clauses;
+            list<unsigned int> column_clauses;
             // iterate over each row
             for (unsigned int j = 0; j < dimension; j++) {
                 column_clauses.push_back(encode(n, i, j, bits));
@@ -106,8 +108,8 @@ std::list<std::list<unsigned int>> col_indices(unsigned int order) {
 /*
  * Returns the indices for each row.
  */
-std::list<std::list<unsigned int>> row_indices(unsigned int order) {
-    std::list<std::list<unsigned int>> at_most_one_indices;
+list<list<unsigned int>> row_indices(unsigned int order) {
+    list<list<unsigned int>> at_most_one_indices;
     unsigned int dimension = order * order;
     unsigned int bits = ceil(log2(dimension));
 
@@ -115,7 +117,7 @@ std::list<std::list<unsigned int>> row_indices(unsigned int order) {
     for (unsigned int n = 1; n <= dimension; n++) {
         // iterate over each row
         for (unsigned int j = 0; j < dimension; j++) {
-            std::list<unsigned int> row_clauses;
+            list<unsigned int> row_clauses;
             // iterate over each column
             for (unsigned int i = 0; i < dimension; i++) {
                 row_clauses.push_back(encode(n, i, j, bits));
@@ -130,8 +132,8 @@ std::list<std::list<unsigned int>> row_indices(unsigned int order) {
 /*
  * Returns the indices for each block.
  */
-std::list<std::list<unsigned int>> block_indices(unsigned int order) {
-    std::list<std::list<unsigned int>> at_most_one_indices;
+list<list<unsigned int>> block_indices(unsigned int order) {
+    list<list<unsigned int>> at_most_one_indices;
     unsigned int dimension = order * order;
     unsigned int bits = ceil(log2(dimension));
 
@@ -141,7 +143,7 @@ std::list<std::list<unsigned int>> block_indices(unsigned int order) {
         for (unsigned int a = 0; a < order; a++) {
             // iterate over each block row
             for (unsigned int b = 0; b < order; b++) {
-                std::list<unsigned int> block_clauses;
+                list<unsigned int> block_clauses;
                 // iterate over each column in a block
                 for (unsigned int col_i = 0; col_i < order; col_i++) {
                     // iterate over each row in a block
@@ -162,10 +164,10 @@ std::list<std::list<unsigned int>> block_indices(unsigned int order) {
 
 int main() {
     unsigned int sudoku_order = 3;
-    std::list<std::list<unsigned int>> list = col_indices(sudoku_order);
-    std::cout << list.front();
-    std::cout << "---" << std::endl;
-    std::cout << list.back();
-    std::cout << decode(2440, sudoku_order).number << std::endl;
+    list<list<unsigned int>> list = col_indices(sudoku_order);
+    cout << list.front();
+    cout << "---" << endl;
+    cout << list.back();
+    cout << decode(2440, sudoku_order).number << endl;
     return 0;
 }
