46aa3110c170999a13d7edd3aaef16bb112c6468
Innoberger <fynn.arnold@pm.me>
2022-11-11 11:06:19 +0100

    implemented parser for .sudoku files

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include "indices.h"
 
 int main() {
-    unsigned int sudoku_order, order_squared;
+    int sudoku_order, order_squared;
     list<string> lines;
 
     for (string line; getline(cin, line);)
@@ -13,15 +13,59 @@ int main() {
         return 1;
     }
 
-    sudoku_order = (unsigned int) stoi(lines.front());
+    sudoku_order = stoi(lines.front());
     order_squared = sudoku_order * sudoku_order;
     lines.pop_front();
 
-    if (lines.size() != sudoku_order * sudoku_order) {
+    if (sudoku_order < 1) {
+        cout << "error reading from stdin: expected sudoku order in line 1 "
+            << "to be a positive integer, got " << sudoku_order << endl;
+        return 1;
+    }
+
+    if (lines.size() != order_squared) {
         cout << "error reading from stdin: after order number line ("
             << sudoku_order << "), there are order * order (" << order_squared
             << ") lines expected, got " << lines.size() << endl;
         return 1;
+    }
+
+    unsigned int lne_ctr = 0;
+
+    while (!lines.empty()) {
+        list<string> columns = tokenize(lines.front(), ' ');
+
+        if (columns.size() != order_squared) {
+            cout << "error reading from stdin: error in line "
+                << (lne_ctr + 2) << ": expected " << order_squared
+                << " columns separated by whitespace, got " << columns.size() << endl;
+            return 1;
+        }
+
+        unsigned int col_ctr = 0;
+
+        while (!columns.empty()) {
+            int elem = stoi(columns.front());
+
+            if (elem < 0 || elem > order_squared) {
+                cout << "error reading from stdin: error in line "
+                    << (lne_ctr + 2) << ", column " << col_ctr
+                    << ": expected integer between 0 and " << order_squared
+                    << ", got " << elem << endl;
+                return 1;
+            }
+
+            /**
+             * TODO:
+             * assignment of bolean variables for SAT solving goes HERE
+             */
+
+            col_ctr++;
+            columns.pop_front();
+        }
+
+        lne_ctr++;
+        lines.pop_front();
     }
 
     return 0;
--- utils.h
+++ utils.h
@@ -26,3 +26,18 @@ ostream &operator<<(ostream &os, const list<string> &list) {
     }
     return os;
 }
+
+list<string> tokenize(string const &str, const char delim) {
+    size_t start;
+    size_t end = 0;
+    list<string> out;
+
+    while ((start = str.find_first_not_of(delim, end)) != string::npos)
+    {
+        end = str.find(delim, start);
+        out.push_back(str.substr(start, end - start));
+    }
+
+    return out;
+}
+ 
